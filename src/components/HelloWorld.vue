401514fdc2281eff562ba7dae6fafedda0f34ffa
shcheshnyak <stas.szczesniak@gmail.com>
2020-07-14 13:21:20 +0100

    Added buttons

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="hello">
+    <img v-bind:src="info"><img/>
+    <button v-on:click="handleclick">Click Me!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      info: "Default Value"
+    }
+  },
+  methods: {
+    handleclick() {
+      this.update_cats()
+    },
+    update_cats() {
+      var self = this
+
+      fetch('https://aws.random.cat/meow')
+        .then(function(response) {
+            if (response.status !== 200) {
+              console.log('Looks like there was a problem. Status Code: ' +
+                response.status);
+              return;
+            }
+
+            // Examine the text in the response
+            response.json().then(function(data) {
+              self.info = data.file
+            });
+        })
+        .catch(function(err) {
+          console.log('Fetch Error :-S', err);
+        });
+    }
+  },
+  mounted () {
+    this.update_cats()
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
